e3d5f478e591c4fa59c2a37d050e70df06cd2e03
Adhithsunil <98397537+Adhithsunil@users.noreply.github.com>
2024-11-03 22:29:00 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:"/luxnaova/",
-})
+  base: '/luxnaovaEcommerce/', // This must match your GitHub repo name exactly
+  build: {
+    outDir: 'dist', // Ensure the output folder is 'dist'
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js', // This should create 'index.js' in 'assets'
+        assetFileNames: 'assets/[name][extname]', // Ensures 'index.css' is in 'assets'
+      },
+    },
+  },
+});
